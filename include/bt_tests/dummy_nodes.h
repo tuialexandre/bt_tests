6e6bb15ea406fb95fde55d52a076aa5a89ae2821
Tui Baraniuk <tuialexandre@gmail.com>
2020-12-09 17:02:58 -0300

    Moved code from header to cpp file

--- include/bt_tests/dummy_nodes.h
+++ include/bt_tests/dummy_nodes.h
@@ -20,12 +20,7 @@ class ThinkWhatToSay : public SyncActionNode
     }
 
     // This Action writes a value into the port "text"
-    NodeStatus tick() override
-    {
-        // the output may change at each tick(). Here we keep it simple.
-        setOutput("text", "The answer is 42" );
-        return NodeStatus::SUCCESS;
-    }
+    NodeStatus tick() override;
 };
 
 // SyncActionNode (synchronous action) with an input port.
@@ -46,20 +41,7 @@ class SaySomething : public SyncActionNode
     }
 
     // As usual, you must override the virtual function tick()
-    NodeStatus tick() override
-    {
-        Optional<std::string> msg = getInput<std::string>("message");
-        // Check if optional is valid. If not, throw its error
-        if (!msg)
-        {
-            throw BT::RuntimeError("missing required input [message]: ", 
-                                   msg.error() );
-        }
-
-        // use the method value() to extract the valid message.
-        std::cout << "Robot says: " << msg.value() << std::endl;
-        return NodeStatus::SUCCESS;
-    }
+    NodeStatus tick() override;
 };
 
 #endif
--- src/dummy_nodes.cpp
+++ src/dummy_nodes.cpp
@@ -1,2 +1,24 @@
 #include "dummy_nodes.h"
 
+
+BT::NodeStatus ThinkWhatToSay::tick()
+{
+    // the output may change at each tick(). Here we keep it simple.
+    setOutput("text", "The answer is 42" );
+    return NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus SaySomething::tick()
+{
+    Optional<std::string> msg = getInput<std::string>("message");
+    // Check if optional is valid. If not, throw its error
+    if (!msg)
+    {
+        throw BT::RuntimeError("missing required input [message]: ", 
+                                msg.error() );
+    }
+
+    // use the method value() to extract the valid message.
+    std::cout << "Robot says: " << msg.value() << std::endl;
+    return NodeStatus::SUCCESS;
+}
